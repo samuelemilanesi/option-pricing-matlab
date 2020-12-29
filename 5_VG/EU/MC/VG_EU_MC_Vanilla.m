10f3a490f5d2babce0b27ee1bb61fd280b4c7f7e
Samuele Milanesi <samuele.milanesi@gmail.com>
2020-12-29 19:27:48 +0100

    VG monte carlo

---
+++ 5_VG/EU/MC/VG_EU_MC_Vanilla.m
@@ -0,0 +1,38 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Price European Call and Put Options under VG model using MC
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; close all; clc;
+
+%% Parameters
+% Market parameters
+r = 0.01;            % riskfree interest rate 
+S0 = 100;            % spot price
+% Model parameters
+sigma = 0.6;
+theta = 0.05;
+kVG   = 0.2;
+% Contract parameters
+T = 1;                  % maturity
+K = S0;  % strike
+
+par = struct('S0',S0,'r',r,'TTM',T,'sigma',sigma,'theta',theta,'kVG',kVG);
+% Discretization parameter
+Nsim = 1e6;             % number of MC simulations 
+
+%% Simulate Underlying Asset
+% Simulate the underlying at time T
+[~,ST] = VG_simulate_asset(par,Nsim,1);
+%% Compute the discounted payoff
+DiscPayoff = exp(-r * T) * max(ST - K, 0);
+
+%% Compute call price and asymptotic CI 
+disp("VG Model - EU Vanilla options price via plain MC:")
+[call_price, ~, call_CI_price] = normfit(DiscPayoff)
+
+%% Compute put price and asymptotic CI via Call-Put Parity
+put_parity = @(call_p) call_p - S0 + K*exp(-r*T);
+
+put_price = put_parity(call_price)
+put_CI_price = put_parity(call_CI_price)
+
+CI_len = call_CI_price(2)-call_CI_price(1)
